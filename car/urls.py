dbd12a6fab43d1a3cd47ae44174cddd666bce40a
Farshad Gholami <gholamifarshad98@yahoo.com>
2024-02-08 17:13:36 +0330

    now some of viewset class funtion are  refidined.

--- car/urls.py
+++ car/urls.py
@@ -4,10 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("person", views.PersonViewSet)
-
+router.register("car", views.CarViewSet)
 
 urlpatterns = [
-    path("get-car",views.car_view),
-    path("information",views.information),
+
 ]
 urlpatterns+=router.urls
--- car/views.py
+++ car/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import viewsets
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import Person, Car
 from .serializers import CarSerializers, PersonSerializers, CarReadSelrilizers
 
@@ -10,23 +10,44 @@ class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializers
     http_method_names = ['get','post','delete','put']
-
+    def list(self, request, *args, **kwargs):
+        objs = super().list(self, request, *args, **kwargs)
+        print ("=== list ===")
+        return objs
+    
+    def create(self, request, *args, **kwargs):
+        obj = super().create(request, *args, **kwargs)
+        print("=== create ===")
+        return obj
+    
+    def update(self, request, *args, **kwargs):
+        obj = super().update(request, *args, **kwargs)
+        instance = self.get_object()
+        print("=== update ===> {}".format(instance.name))
+        return obj
         
+    def retrieve(self, request, *args, **kwargs):
+        obj = super().retrieve(request, *args, **kwargs)
+        instance = self.get_object()
+        print("=== retrieve ===> {}".format(instance.name))
+        return obj
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        print("=== destroy ===> {}".format(instance.name))
+        obj = super().destroy(request, *args, **kwargs)
+        return obj
 
-@api_view(["GET", "POST"])
-def car_view(request):
-    if request.method == "GET":
-        cars = Car.objects.all()
-        return Response(CarSerializers(cars, many= True).data, status=status.HTTP_200_OK)
-    elif request.method == "POST":
-        ser = CarSerializers(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data, status=status.HTTP_201_CREATED)
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    http_method_names = ["post", "get", "delete", "put"]
+    
+    def get_serializer_class(self):
+        if self.request.method not in permissions.SAFE_METHODS:
+            return CarSerializers
         else:
-            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-@api_view(["GET"])
-def information(request):
-    cars = Car.objects.all()
-    return Response(CarReadSelrilizers(cars, many= True).data, status=status.HTTP_200_OK)
+            return CarReadSelrilizers
+
+
+
